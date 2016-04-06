f9d53a8023d882697b99b97bbec3f19c2364f6ac
thervig <thervig@asu.edu>
2016-04-05 20:50:12 -0700

    Update point.py

--- files/point.py
+++ files/point.py
@@ -10,19 +10,19 @@ class Point(object):
       self.mark = mark
       
   def __eq__(self, other):
-		return self.x == other.x and self.y == other.y
+      return self.x == other.x and self.y == other.y
 
-	def __add__(self, other):
-		return Point(self.x + other.x, self.y + other.y)
+  def __add__(self, other):
+      return Point(self.x + other.x, self.y + other.y)
 
-	def __ne__(self, other):
-		return self.x != other.x or self.y != other.y
+  def __ne__(self, other):
+      return self.x != other.x or self.y != other.y
 	
-	def __str__(self):
-		return ("({0}, {1}").format(self.x, self.y)
+  def __str__(self):
+      return ("({0}, {1}").format(self.x, self.y)
 
   def array(self):
-    return [self.x, self.y]
+      return [self.x, self.y]
     
   def point_check_coincident(self, test):
       return utils.check_coincident((self.x, self.y), test)
